21fd9d4c2c060c8853ea389f79290c30de5c78cf
Nurdaulet <artykbaev.nurdaulet@mail.ru>
2022-09-09 18:02:21 +0600

    first commit

---
+++ web/helpers.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+// Обработчик главной странице.
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	// Инициализируем срез содержащий пути к двум файлам. Обратите внимание, что
+	// файл home.page.tmpl должен быть *первым* файлом в срезе.
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	// Используем функцию template.ParseFiles() для чтения файла шаблона.
+	// Если возникла ошибка, мы запишем детальное сообщение ошибки и
+	// используя функцию http.Error() мы отправим пользователю
+	// ответ: 500 Internal Server Error (Внутренняя ошибка на сервере)
+	ts, err := template.ParseFiles(files...)
+	//ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// Затем мы используем метод Execute() для записи содержимого
+	// шаблона в тело HTTP ответа. Последний параметр в Execute() предоставляет
+	// возможность отправки динамических данных в шаблон.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+	//w.Write([]byte("Привет из Snippetbox"))
+}
+
+// Обработчик для отображения содержимого заметки.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	// Извлекаем значение параметра id из URL и попытаемся
+	// конвертировать строку в integer используя функцию strconv.Atoi(). Если его нельзя
+	// конвертировать в integer, или значение меньше 1, возвращаем ответ
+	// 404 - страница не найдена!
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Используем функцию fmt.Fprintf() для вставки значения из id в строку ответа
+	// и записываем его в http.ResponseWriter.
+	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
+}
+
+// Обработчик для создания новой заметки.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	// Используем r.Method для проверки, использует ли запрос метод POST или нет. Обратите внимание,
+	// что http.MethodPost является строкой и содержит текст "POST".
+	if r.Method != http.MethodPost {
+		// Используем метод Header().Set() для добавления заголовка 'Allow: POST' в
+		// карту HTTP-заголовков. Первый параметр - название заголовка, а
+		// второй параметр - значение заголовка.
+		w.Header().Set("Allow", http.MethodPost)
+
+		// Используем функцию http.Error() для отправки кода состояния 405 с соответствующим сообщением.
+		http.Error(w, "Метод запрещен!", 405)
+
+		// Затем мы завершаем работу функции вызвав "return", чтобы
+		// последующий код не выполнялся.
+		return
+	}
+
+	w.Write([]byte("Создание новой заметки..."))
+}
